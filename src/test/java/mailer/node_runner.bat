3b4f71de213f46d7a7edee70c9c689b24f894f3a
Dhinesh.p <testing.iddhi@gmail.com>
2024-10-09 18:10:04 +0530

    updated on 09-10-22

--- src/test/java/mailer/node_runner.bat
+++ src/test/java/mailer/node_runner.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:\Users\Dhinesh.P\OneDrive - KNOWLEDGEQ INTERACTIVE CONSULTANCY SERVICE PVT LTD\Desktop\Java\dvtraining\src\test\java\mailer
+cd ..\src\test\java\mailer
 node server.js
 pause
